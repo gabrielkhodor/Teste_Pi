0c9f8bcda824321f9a8569c4eb1edae95f0f7541
ppconrado <ppconrado@yahoo.com.br>
2026-05-25 02:27:40 -0300

    Add initial backend structure with Express setup and .gitignore

---
+++ back-end/src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'API Express funcionando',
+    version: 'v1',
+  });
+});
+
+app.get('/api/v1/health', (req, res) => {
+  res.json({
+    status: 'ok',
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Rota não encontrada',
+  });
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+
+  res.status(500).json({
+    error: 'Erro interno do servidor',
+  });
+});
+
+module.exports = app;
